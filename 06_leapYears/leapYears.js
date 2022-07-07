e9c163ea6392af8dd03eda82596a139cc71574ea
dholm90 <devanholm@gmail.com>
2022-07-07 14:06:01 -0400

    Finished leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    let result = false;
+    if (typeof year == "number" && year > 0 && year % 4 == 0 && year % 100 || year % 400 == 0) {
+        result = true;
+        return result;
+    }
+    else {
+        return result;
+    }
 };
 
 // Do not edit below this line
