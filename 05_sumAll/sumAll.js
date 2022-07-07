15928acc87eb6765ba37f50d5aeab5580f2a2258
dholm90 <devanholm@gmail.com>
2022-07-07 13:50:23 -0400

    Finished sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (number, arrLength) {
+    let sum = 0;
 
+    if (typeof number != "number" || typeof arrLength != "number") return "ERROR";
+    else if (number < 0 || arrLength < 0) return "ERROR";
+    else if (number < arrLength) {
+        for (i = number; i <= arrLength; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (i = arrLength; i <= number; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
